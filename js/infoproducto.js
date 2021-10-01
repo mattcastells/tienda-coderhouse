023a272e3e57d1d7f2c11093505a2edf96fc8e96
mattcastells <gulinmatias96@gmail.com>
2021-09-30 22:37:19 -0300

    big-changes-for-js

---
+++ js/infoproducto.js
@@ -0,0 +1,20 @@
+let contenedorProductos = document.getElementById('contenedorgeneral')                                                     // Se genera una variable y se le asocia un elemento del html
+
+function recuperar() {                                                                                                     // Se declara la funcion para recuperar datos
+    let recuperado = JSON.parse(localStorage.getItem('datos producto'))                                                    // Se genera una variable y se le asigna el valor del objeto JSON que se encuentra en el storage. Se lo parsea
+    console.log(recuperado)
+    if(recuperado){                                                                                                        // Condicional
+        recuperado.forEach(el => {                                                                                         // Se itera sobre cada elemento y posteriormente se printean en el html
+            contenedorProductos.innerHTML=` <div id="contenedor__pag__producto">                                           
+                                            <img id="foto__pag__producto" src="${el.img}" alt="${el.nombre}"></img>
+                                            <div id="contenedor__pag__info">
+                                                <h3 id="nombre__pag__producto"> ${el.nombre}</h3>
+                                                <h5 id="generos__pag__producto"> ${el.genero}</h5>
+                                                <p id="descripcion__pag__producto">${el.descripcion}</p>
+                                                <button class="boton__pag__producto">Añadir al carrito</button>
+                                            </div>
+                                            </div>`
+        });
+    }
+}
+recuperar();                                                                                                                // Cuando se genera la pagina, se llama a la funcion recuperar para que la autocomplete
--- js/main.js
+++ js/main.js
@@ -1,80 +1,43 @@
-// Creacion del carrito como variable
-let carritoCompras = []  // Es establecido como un array para pushear las variables de los productos dentro
-let carritoPrecio = []
+// Imprimir juegos en index.html
 
-function agregarCarrito () {
+let contenedorProductos = document.getElementById('product__container');                                    // Se crea la variable para asignarle el contenedor del html
+mostrarJuegos(stockJuegos);
 
-    let eleccionProducto = "";  
-    let eleccionTipoProducto = "";
+function mostrarJuegos(array) {  
+    for (const juego of array) {                                                                            // Para cada juego en stockJuegos
 
-    do {
+        let plantilla = document.createElement('div');                                                      // Se crea divisor
+        plantilla.setAttribute("class", "product__box");                                                    // Se le asigna la clase al divisor
 
-    // Se le solicita al cliente el tipo de producto que deseaconsolas
-    
-    eleccionTipoProducto = prompt("Que producto esta buscando?\n juegos\n consolas");
+        plantilla.innerHTML = `<a class="clickImage" id=${juego.id} href="/pages/producto.html" target="blank" ><img class="product__img" src="${juego.img}" alt="${juego.nombre}"></img></a>
+                            <h4 class="product__title"> ${juego.nombre}</h4>
+                            <h5 class="product__price">$ ${juego.precio}</h5>
+                            <button class="product__button">Añadir al carrito</button>`;
+                            
+        contenedorProductos.appendChild(plantilla)                                                          // Se agrega la plantilla del juego al contenedor
 
-    } while ((eleccionTipoProducto != "juegos") && (eleccionTipoProducto === "consolas") && (eleccionTipoProducto === "ESC"));
-
-    if (eleccionTipoProducto === "juegos") {
-
-        do {
-
-            eleccionProducto = prompt("Que juegos esta buscando?\n Para dejar de agregar juegos presione ESC");
-
-            // El producto se filtra y es agregado al carrito
-            let agregarProducto = stockJuegos.filter((parametro) => parametro.nombre == eleccionProducto) [0]
-            carritoCompras.push(agregarProducto)
-
-            let agregarPrecioProductos = stockJuegos.filter((parametro) => parametro.precio) [0];
-            carritoPrecio.push(agregarPrecioProductos)
-
-            // Se muestra en consola para verificar su funcionamiento
-            console.log(carritoCompras);
-            console.log(eleccionProducto)
-
-            // Se actualiza el carrito para que el cliente pueda ver el total de sus elecciones
-            actualizarCarrito()
-
-        } while (eleccionProducto != "listo")
-
-    } else if (eleccionTipoProducto === "consolas") {
-
-        do {
-
-            eleccionProducto = prompt("Que consolas esta buscando?\n Para dejar de agregar consolas presione ESC");
-
-            // El producto se filtra y es agregado al carrito
-            let agregarProducto = stockConsolas.filter((parametro) => parametro.nombre == eleccionProducto) [0]
-            carritoCompras.push(agregarProducto)
-
-            let agregarPrecioProductos = stockConsolas.filter((parametro) => parametro.precio) [0];
-            carritoPrecio.push(agregarPrecioProductos)
-        
-            // Se muestra en consola para verificar su funcionamiento
-            console.log(carritoCompras);
-            console.log(eleccionProducto)
-        
-            // Se actualiza el carrito para que el cliente pueda ver el total de sus elecciones
-            actualizarCarrito()
-
-        } while (eleccionProducto != "ESC")
-
-    } else {
-        alert('El parametro indicado no existe')
     }
-}
-
-function actualizarCarrito () {
-
-    // Se muestra en la consola el numero total de productos seleccionados por el cliente
-    console.log("Numero de objetos en su carrito " + carritoCompras.length);
-
-    // Se genera la suma de los elementos
-    let sumaTotal = carritoPrecio.reduce((total, producto) => total + producto.precio, 0)
-
-    // Se muestra en consola el total de la suma junto a la string establecida
-    console.log("El total de su compra es de " + sumaTotal);
 
 }
 
-agregarCarrito ()
+
+// Capturar datos de los juegos y guardarlos en el storage
+
+function capturarDatos (id) {                                                                               // Se declara una funcion que capture los datos
+
+    let juego = stockJuegos.find(prod => prod.id == id)                                                     // Se itera en el array general de productos y se le asigna un valor a la variable juego, identificando al mismo por su id
+
+    let productoInfo = [];                                                                                  // La variable se declara con scope local para que su valor se pierda (sea reutilizable)
+    productoInfo.push(juego)                                                                                // Se pushea la variable juego (id) dentro del array establecido arriba
+    console.log(productoInfo)
+
+     localStorage.setItem('datos producto', JSON.stringify(productoInfo));                                  // Se guarda la informacion del array productoInfo como un objeto JSON en el local storage
+
+}
+
+let tomarDatos = document.querySelectorAll('.clickImage');                                                  // Se le asigna un elemento del html a la variable tomarDatos
+ tomarDatos.forEach((producto) => producto.addEventListener("click",()=>{                                   // Se le agrega el evento click y la funcionalidad de capturar datos a todos los elementos con la clase "clickImage" (Imagenes de los productos)
+     capturarDatos(producto.id)
+ }) )
+
+ // mostrarJuegos(stockJuegos.filter(el => el.genero.find(el=>el == 'rpg')))
