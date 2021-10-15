792b605e6310213070e96b7c999cc01f4537ab5e
mattcastells <gulinmatias96@gmail.com>
2021-10-14 22:41:13 -0300

    add-stock.json

--- js/carrito.js
+++ js/carrito.js
@@ -1,23 +1,56 @@
-// Sin funcionar todavia
+// // Sin funcionar todavia
 
-let carritoDeCompras = [];
+// let carritoDeCompras = [];
 
-let botonAgregarCarrito = document.getElementsByClassName('boton__producto');
+// let botonAgregarCarrito = document.getElementsByClassName('boton__producto');
 
 
 
-function agregarAlCarrito(informacionDelJuego) {
+// function agregarAlCarrito(informacionDelJuego) {
 
-    informacionDelJuego = stockJuegos.find(prod => prod.id == id);
+//     informacionDelJuego = stockJuegos.find(prod => prod.id == id);
 
-    let infoProducto = [];
-    infoProducto.push(producto)
+//     let infoProducto = [];
+//     infoProducto.push(producto)
 
-    console.log(infoProducto);
+//     console.log(infoProducto);
 
-}
+// }
 
-function actualizarCarrito() {}
+// function actualizarCarrito() {}
 
 
 // botonAgregarCarrito.addEventListener("click", agregarAlCarrito)
+
+
+// Imprimir en carrito
+
+let contenedorCarrito = document.getElementById('contenedorCarrito');
+
+function recuperarCarrito() {
+    let recuperado = JSON.parse(localStorage.getItem('datos producto'))                                                    // Se genera una variable y se le asigna el valor del objeto JSON que se encuentra en el storage. Se lo parsea
+    console.log(recuperado)
+    if(recuperado){                                                                                                        // Condicional
+        recuperado.forEach(el => {                                                                                         // Se itera sobre cada elemento y posteriormente se printean en el html
+            contenedorCarrito.innerHTML=` <div class="contenedor__pag__carrito">                                           
+                                            <img class="foto__pag__carrito" src=".${el.img}" alt="${el.nombre}"></img>
+                                            <div class="contenedor__pag__carrito">
+                                                <h3 class="nombre__pag__carrito"> ${el.nombre}</h3>
+                                                <h3 class="precio__pag__carrito">$ ${el.precio}</h3>
+                                                <button class="eliminar__producto"> Quitar </button>
+                                            </div>
+                                            </div>`
+        });
+    }
+
+}
+
+recuperarCarrito();
+
+
+
+let precioFinal = document.getElementById('precioFinal');
+
+function sumaFinal (precio) {
+
+}
--- js/main.js
+++ js/main.js
@@ -1,5 +1,22 @@
+let stockProductos = [];                                                                                     // Se crea un array vacio en el que pushear los datos de los juegos                                                                         
+
+function recuperarStock() {
+    let stock = JSON.parse(localStorage.getItem('stock'))                                                    // Se parsean los datos del json guardados en el storage
+    if(stock) {                                                                                              // Se comprueba si es verdadero
+        stock.forEach(el => stockProductos.push(el))                                                         // Se pushea en el array stockProductos la informacion de los juegos
+    }
+}
+
+$.getJSON('js/stock.json', function (data) {                                                                 // Se utiliza Ajax para traer los productos desde el archivo stock.json
+    localStorage.setItem('stock', JSON.stringify(data))                                                      // Se guardan los datos del json en local storage
+    recuperarStock()                                                                                         // Se recuperan los datos desde el storage al ser parseados
+    mostrarJuegos(data)                                                                                      // Se llama a la funcion para que muestre los juegos trayendolos desde el localStorage
+    
+})
+
+
 // Imprimir juegos en index.html                                 
-mostrarJuegos(stockJuegos);                                                                                   // Se llama a la funcion para que se ejecute y muestre los productos
+// mostrarJuegos(stockProductos);                                                                             // Se llama a la funcion para que se ejecute y muestre los productos
 
 function mostrarJuegos(array) {  
     for (const juego of array) {                                                                              // Para cada juego en stockJuegos
@@ -8,14 +25,14 @@ function mostrarJuegos(array) {
                             <a class="clickImage" id=${juego.id} href='pages/producto.html' target="blank"><img class="imagen__producto" src="${juego.img}" alt="${juego.nombre}"></img></a>
                             <h4 class="titulo__producto"> ${juego.nombre}</h4>
                             <h5 class="precio__producto">$ ${juego.precio}</h5>
-                            <button class="boton__producto">Añadir al carrito</button>
+                            <button class="boton__producto botonCarrito">Añadir al carrito</button>
             </div>`)                            
     }
 }
 
-// Capturar datos de los juegos y guardarlos en el storage
+// Capturar datos de los juegos y guardarlos en el storage para re-imprimirlos en productos.html
 
-function capturarDatos (id) {                                                                               // Se declara una funcion que capture los datos
+function capturarDatos (id) {                                                                               // Se declara una funcion que capture los datos para generar contenido en producto.html
 
     let juego = stockJuegos.find(prod => prod.id == id)                                                     // Se itera en el array general de productos y se le asigna un valor a la variable juego, identificando al mismo por su id
 
@@ -27,29 +44,58 @@ function capturarDatos (id) {                                                   
 
 }
 
-let tomarDatos = document.querySelectorAll('.clickImage');                                                  // Se le asigna un elemento del html a la variable tomarDatos
- tomarDatos.forEach((producto) => producto.addEventListener("click",()=>{                                   // Se le agrega el evento click y la funcionalidad de capturar datos a todos los elementos con la clase "clickImage" (Imagenes de los productos)
+
+// Capturar datos de los juegos seleccionados y agregarlos al carrito, para posteriormente re-imprimirlos en compras.html
+
+let productoCarrito = []; // La variable se declara con scope global para que su valor no se pierda 
+
+function capturarDatosCarrito (id) {                                                                        // Se declara una funcion que capture los datos para el carrito, compras.html
+
+    let productoSeleccionado = stockJuegos.find(prod => prod.id == id)                                      // Se itera en el array general de productos y se le asigna un valor a la variable juego, identificando al mismo por su id
+                                                                                      
+    productoCarrito.push(productoSeleccionado)                                                              // Se pushea la variable juego (id) dentro del array establecido arriba (productoCarrito)
+    console.log(productoCarrito)
+
+     localStorage.setItem('Producto carrito', JSON.stringify(productoCarrito));                             // Se guarda la informacion del array productoCarrito como un objeto JSON en el local storage
+
+}
+
+// Tomar datos de los productos a partir de su imagen en index.html para recuperarlos en producto.html
+
+let tomarDatos = document.querySelectorAll('.clickImage',);                                                  // Se le asigna un elemento del html a la variable tomarDatos
+ tomarDatos.forEach((producto) => producto.addEventListener("click",()=>{                                    // Se le agrega el evento click y la funcionalidad de capturar datos a todos los elementos con la clase "clickImage" (Imagenes de los productos)
      capturarDatos(producto.id)
  }) )
- 
+
+ // Tomar datos de los productos a partir del boton de compra en index.html para recuperarlos en compras.html
+
+ let tomarDatosCarrito = document.querySelectorAll('.botonCarrito');                                         // Se le asigna un elemento del html a la variable tomarDatos
+ tomarDatosCarrito.forEach((producto) => producto.addEventListener("click",()=>{                             // Se le agrega el evento click y la funcionalidad de capturar datos a todos los elementos con la clase "clickImage" (Imagenes de los productos)
+     capturarDatosCarrito(producto.id)
+ }) )
  
 
-// Filtrar mediante checkboxes
+ 
 
-function checkbox() {
-    let input_checkbox_filtro = document.btn__filtro.filtro;
+
+
+
+// Filtrar mediante checkboxes en el index
+
+function checkbox() {                                                                                        // Se declara la funcion checkbox
+    let input_checkbox_filtro = document.btn__filtro.filtro;                                                 // Se le asocia un elemento del html a la variable
     
-    input_checkbox_filtro.forEach(el=>{
+    input_checkbox_filtro.forEach(el=>{                                                                      // Por cada elemento en
 
-        el.addEventListener('change', function(){
+        el.addEventListener('change', function(){                                                            // Se le agrega un listener de cambio al parametro elemento
             for (let i = 0; i < input_checkbox_filtro.length; i++) {
                
-                if(input_checkbox_filtro[i].checked){
+                if(input_checkbox_filtro[i].checked){                                                        // El condicional establece que si el elemento se encuentran tildado/checked
                     let grupo = input_checkbox_filtro[i].value;
                     
-                    if(grupo == 'todos' || grupo == false){
+                    if(grupo == 'todos' || grupo == false){                                                  // Si la caja todos, o ninguna caja se encuentra tiltada, se muestran todos los juegos
                         mostrarJuegos(stockJuegos)
-                    }else{
+                    }else{                                                                                   // Caso contrario, se muestran los juegos que coincidan con el parametro establecido por el usuario mediante las cajas
                       const findGrupo = stockJuegos.filter(el => el.genero.find(el => el == grupo));
                     mostrarJuegos(findGrupo)
                       
@@ -65,26 +111,16 @@ checkbox()
 
 // Filtrar mediante searchbar
 
-// let search_input  = document.getElementById('searchBar');
-// let search_term  = "";
+$('#searchBar').on('keyup',buscar)                                                                              // Se asocia la barra de busqueda junto a la funcion buscar
 
-// search_input.addEventListener('input', e => {
-
-// // Guardar el valor de input
-// search_term = e.target.value;
-
-// // Redesplegar juegos segun el valor de search_term
-// mostrarJuegoss()
-
-// });
-
-// let contenedorProductos = document.getElementById('contenedor__producto');
-
-// const mostrarJuegoss = async () => {
-//     // Limpiar resultados
-//     contenedorProductos.innerHTML = "";
-// }
-
-
+function buscar(e){                                                                                             // Se declara la funcion buscar y se le pasa un parametro e
+    e.preventDefault()                                                                                          // Se previene la carga automatica del parametro
+    console.log(e.target.value);
+    let texto = e.target.value.toLowerCase()                                                                    // Se declara la variable texto, y se le asigna como valor los datos introducidos por el usuario, al mismo tiempo se transforma a minuscula
+    let buscar = stockJuegos.filter(prod => prod.nombre.toLocaleLowerCase().includes(texto))                    // Se declara la variable buscar y se le asocia un filtro en stockJuegos
+    console.log(buscar);
+    $('#contenedor__producto').empty()                                                                          // Se vacia el contenedor producto
+    mostrarJuegos(buscar)                                                                                       // Se muestran los juegos que coincidan con el valor introducido en buscar
+}
 
 
